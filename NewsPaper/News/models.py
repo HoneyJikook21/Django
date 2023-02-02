c34b4b52e1e0baeda84bbe0c409de6f5e5d93f87
Vika <astakhova.vicka@yandex.ru>
2023-02-02 22:27:42 +0300

    add files

--- NewsPaper/News/models.py
+++ NewsPaper/News/models.py
@@ -1,53 +1,74 @@
 from datetime import datetime
 
 from django.db import models
-from News.resources import POSITIONS, editor
+from django.contrib.auth.models import User
+from django.db.models import Sum
 
-class Order(models.Model):
-    time_in = models.DateTimeField(auto_now_add=True)
-    time_out = models.DateTimeField(null=True)
-    cost = models.FloatField(default=0.0)
-    take_away = models.BooleanField(default=False)
-    complete = models.BooleanField(default=False)
-    staff = models.ForeignKey('Staff', on_delete=models.CASCADE)
-    products = models.ManyToManyField('Product', through='ProductOrder')
 
-    def finish_order(self):
-        self.time_out = datetime.now()
-        self.complete = True
+class Author(models.Model):
+    authorUser = models.OneToOneField(User,on_delete=models.CASCADE)
+    ratingAuthor = models.SmallIntegerField(default=0)
+
+    def update_rating(self):
+        postRat = self.post_set.aggregate(postRating=Sum('rating'))
+        pRat = 0
+        pRat += postRat.get('postRating')
+
+        commentRat = self.authorUser.comment_set.aggregate(commentRating=Sum('rating'))
+        cRat = 0
+        cRat += commentRat.get('commentRating')
+
+        self.ratingAuthor = pRat * 3 + cRat
         self.save()
 
-    def get_duration(self):
-        if self.complete:
-            return (self.time_out - self.time_in).total_seconds() // 60
-        else:
-            return (datetime.now() - self.time_in).total_seconds() // 60
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.FloatField(default=0.0)
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    NEWS = 'NW'
+    ARTICLE = 'AR'
+    CATEGORY_CHOICES = (
+        (NEWS, 'Новость'),
+        (ARTICLE, 'Статья')
+
+    )
+    categoryType = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default=ARTICLE)
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    postCategory = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    rating = models.SmallIntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return self.text[0:123]+'...'
 
 
-class Staff(models.Model):
-    full_name = models.CharField(max_length=255)
-    position = models.CharField(max_length=2, choices=POSITIONS, default=editor)
-    labor_contract = models.IntegerField()
+class PostCategory(models.Model):
+    postThrough = models.ForeignKey(Post, on_delete=models.CASCADE)
+    categoryThrough = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
-class ProductOrder(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    _amount = models.IntegerField (default=1, db_column='amount')
+class Comment(models.Model):
+    commentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
+    commentUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    rating = models.SmallIntegerField(default=0)
 
-    def product_sum(self):
-       return self.product.price * self.amount
+    def like(self):
+        self.rating += 1
+        self.save()
 
-    @property
-    def amount(self):
-        return self._amount
-
-    @amount.setter
-    def amount(self, value):
-        self.amount = int(value) if value >= 0 else 0
+    def dislike(self):
+        self.rating -= 1
         self.save()
